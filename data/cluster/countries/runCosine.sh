ae44808d48c0c9f0f63d04fa5807e1d4f7d40375
Anne Flinchbaugh <cs.utexas.edu>
2013-02-22 02:18:02 -0600

    Final code changes

---
+++ data/cluster/countries/runCosine.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+EUC=$'\nEuclidean'
+MID=$'\n\nNEW DISTANCE\n\n'
+COS=$'\nCosine'
+#for num in 1 2 3 4 5 6 7 8 9 10
+#   do
+#	echo "$EUC $num"
+#	anlp app Cluster -k $num -c -d euclidean -t z -r -f countries birth.dat
+#done
+
+#echo "$MID"
+
+
+for num in 1 2 3 4 5 6 7 8 9 10
+   do 
+	echo "$COS $num"
+	anlp app Cluster -k $num -c -t z -r -f countries birth.dat
+done
+
+
+
+
+
